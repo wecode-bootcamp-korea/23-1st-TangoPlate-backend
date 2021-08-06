1b836ccd36aacd9377564fdcd4fb3fd16be269d6
lunayyko <youryoung.ko@gmail.com>
2021-08-06 20:44:27 +0900

    EDIT: 로그인데코레이터삭제, 안쓰는 주석 삭제, 식당세부정보뷰정리

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views      import RestaurantDetailView
 
 urlpatterns = [
-    # path("/restaurant/?no=<int:restaurant_id>", RestaurantDetailView.as_view()),
-    path("/<int:restaurant_id>", RestaurantDetailView.as_view())
+    path("/<int:restaurant_id>", RestaurantDetailView.as_view()),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -10,16 +10,15 @@ from reviews.models       import Review, ReviewImage
 from users.utils          import login_decorator
 
 class RestaurantDetailView(View):
-    @login_decorator
     def get(self, request, restaurant_id):
         try:
-            restaurant        = Restaurant.objects.get(id = restaurant_id)
-            reviews           = Review.objects.filter(restaurant_id = restaurant_id)
-            is_wished         = restaurant.wishlist_set.filter(restaurant_id=restaurant_id).exists()
-            menus             = restaurant.menu_set.filter(restaurant_id=restaurant.id)
-            retaurant_ratings = Rating.objects.filter(restaurant_id=restaurant_id)
-            user_rating       = Rating.objects.get(user_id = request.user.id, restaurant_id=restaurant_id)
-            rating_num = retaurant_ratings.aggregate(rating = Avg('rating'))['rating']
+            restaurant         = Restaurant.objects.get(id = restaurant_id)
+            reviews            = Review.objects.filter(restaurant_id = restaurant_id)
+            is_wished          = restaurant.wishlist_set.filter(restaurant_id=restaurant_id).exists()
+            menus              = restaurant.menu_set.filter(restaurant_id=restaurant.id)
+            restaurant_ratings = Rating.objects.filter(restaurant_id=restaurant_id)
+            user_rating        = Rating.objects.get(user_id = request.user.id, restaurant_id=restaurant_id)
+            rating_num         = restaurant_ratings.aggregate(rating = Avg('rating'))['rating']
 
             results = []
             results.append({
@@ -31,16 +30,21 @@ class RestaurantDetailView(View):
                 "category"       : restaurant.category.name,
                 "location"       : restaurant.location.area,
                 "serving_price"  : restaurant.serving_price.price,
-                "menus"          : [{"item" : menu.item, "item_price" : int(menu.item_price)} for menu in menus],
+                "menus"          : [{   
+                                        "item"       : menu.item, 
+                                        "item_price" : menu.item_price
+                                    } for menu in menus],
                 "is_wished"      : is_wished,
                 "review"         : [{
-                    "review_id"  : review.id,
-                    "user_id"    : review.user.id,
-                    "user_name"  : review.user.nickname,
-                    "description": review.description,
-                    "rating"     : user_rating.rating,
-                    "created_at" : review.created_at,
-                    "images"     : [{"image_url" : imageobject.image} for imageobject in ReviewImage.objects.filter(review_id=review.id)]
+                                        "review_id"  : review.id,
+                                        "user_id"    : review.user.id,
+                                        "user_name"  : review.user.nickname,
+                                        "description": review.description,
+                                        "rating"     : user_rating.rating,
+                                        "created_at" : review.created_at,
+                                        "images"     : [{
+                                                            "image_url" : imageobject.image
+                                                        } for imageobject in ReviewImage.objects.filter(review_id=review.id)]
                 } for review in reviews]
             })
             return JsonResponse({"results": results}, status=201)
