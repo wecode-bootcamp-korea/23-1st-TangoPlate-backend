a77f41fa2204093546b9a49280d9be37176e2d72
lunayyko <youryoung.ko@gmail.com>
2021-08-06 15:06:31 +0900

    ADD: 식당세부정보 뷰

--- restaurants/views.py
+++ restaurants/views.py
@@ -2,38 +2,30 @@ from django.shortcuts import render
 
 from django.views         import View
 from django.http          import JsonResponse
+from django.db.models     import Avg
 
 from restaurants.models   import Restaurant, Category, Location, ServingPrice, Menu
 from users.models         import User, WishList, Rating
-from reviews.models       import Review
+from reviews.models       import Review, ReviewImage
 from users.utils          import login_decorator
 
 class RestaurantDetailView(View):
     @login_decorator
     def get(self, request, restaurant_id):
         try:
-            restaurant = Restaurant.objects.get(id = restaurant_id)
-            reviews = Review.objects.filter(restaurant_id = restaurant_id)
-            is_wished  = restaurant.wishlist_set.filter(restaurant_id=restaurant_id).exists()
+            restaurant        = Restaurant.objects.get(id = restaurant_id)
+            reviews           = Review.objects.filter(restaurant_id = restaurant_id)
+            is_wished         = restaurant.wishlist_set.filter(restaurant_id=restaurant_id).exists()
+            menus             = restaurant.menu_set.filter(restaurant_id=restaurant.id)
+            retaurant_ratings = Rating.objects.filter(restaurant_id=restaurant_id)
+            user_rating       = Rating.objects.get(user_id = request.user.id, restaurant_id=restaurant_id)
+            rating_num = retaurant_ratings.aggregate(rating = Avg('rating'))['rating']
+
             results = []
-            menus = restaurant.menu_set.filter(restaurant_id=restaurant.id)
-            
-            # ratings = Rating.objects.filter(restaurant_id=restaurant_id)
-            # rating_list = [rating for rating in ratings]
-            # a = []
-            # for rating in rating_list:
-            #     a.append(rating.rating)
-            # rating = sum(a)/len(a)
-
-            #Ratings.aggregate(Avg('rating'))
-
-            # nickname = review.user_id.users.nickname
-            # [review.user for review in reviews]
-
             results.append({
                 "id"             : restaurant.id,
                 "name"           : restaurant.name,
-                # "rating"         : rating,
+                "rating"         : rating_num,
                 "address"        : restaurant.address,
                 "phone_number"   : restaurant.phone_number,
                 "category"       : restaurant.category.name,
@@ -42,14 +34,13 @@ class RestaurantDetailView(View):
                 "menus"          : [{"item" : menu.item, "item_price" : int(menu.item_price)} for menu in menus],
                 "is_wished"      : is_wished,
                 "review"         : [{
-                    "user_id"    : review.user_id,
-                    # "user_name"  : nickname,
+                    "review_id"  : review.id,
+                    "user_id"    : review.user.id,
+                    "user_name"  : review.user.nickname,
                     "description": review.description,
-                    # "rating"     : restaurant.ratings_set.rating,
+                    "rating"     : user_rating.rating,
                     "created_at" : review.created_at,
-                    "images"     : {
-                        # review.review_images_set.image
-                    }
+                    "images"     : [{"image_url" : imageobject.image} for imageobject in ReviewImage.objects.filter(review_id=review.id)]
                 } for review in reviews]
             })
             return JsonResponse({"results": results}, status=201)
