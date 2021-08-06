ea09bdc96b950fe2e3c3899ffc515133981cb7de
lunayyko <youryoung.ko@gmail.com>
2021-08-06 15:43:53 +0900

    EDIT: urls.py안쓰는 부분 지움

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
-from .views      import ReviewView
 
 urlpatterns = [
-    # path("/restaurant/?no=<int:restaurant_id>", RestaurantDetailView.as_view()),
-    path("/<int:restaurant_id>", ReviewView.as_view()),
 ]
