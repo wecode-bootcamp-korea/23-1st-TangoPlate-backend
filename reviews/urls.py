5848c09f2080d4a0d54d2fd2d2a0ba428769938a
Samdaso-o <soc05214@naver.com>
2021-08-10 15:35:08 +0900

    EDIT:view class안에 post,delete,put handler method 구현

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,8 @@
-from reviews.models import Review
+from reviews.views  import ReviewView
+
 from django.urls    import path
 
-from reviews.views  import ReviewCreatView, ReviewUpdateDeleteView\
-
 urlpatterns = [
-    path('/<int:restaurant_id>/review', ReviewCreatView.as_view()),
-    path('/<int:restaurant_id>/review/<int:review_id>', ReviewUpdateDeleteView.as_view()), 
+    path('/<int:restaurant_id>/review', ReviewView.as_view()),
+    path('/<int:restaurant_id>/review/<int:review_id>', ReviewView.as_view()),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -9,70 +9,58 @@ from users.models           import User, WishList
 from users.utils            import login_decorator
 from restaurants.models     import Restaurant
 
-class ReviewCreatView(View):
+class ReviewView(View):
     @login_decorator
     def post(self, request, restaurant_id):
-        try:
-            data    = json.loads(request.body)
-            user    = request.user
+        data    = json.loads(request.body)
+        user    = request.user
 
-            if Review.objects.filter(user_id = user.id, restaurant_id=restaurant_id).exists():
-                return JsonResponse({'MESSAGE':'REVIEW_EXIST'}, status=400)
+        if Review.objects.filter(user_id = user.id, restaurant_id=restaurant_id).exists():
+            return JsonResponse({'MESSAGE':'REVIEW_EXIST'}, status=400)
 
-            review = Review.objects.create(
+        review = Review.objects.create(
+            restaurant_id = restaurant_id,
+            user_id       = user.id,
+            description   = data['description'],
+            rating        = data['rating'],
+        )
+
+        ReviewImage.objects.create(
+            review_id = review.id,
+            image     = data['image'],
+        )
+
+        return JsonResponse({'MESSAGE':'SUCCESS'},status = 201)
+
+    @login_decorator
+    def patch(self, request, restaurant_id, review_id):
+        data = json.loads(request.body)
+        user = request.user
+
+        if not Review.objects.filter(id=review_id).exists():
+            return JsonResponse({'MESSAGE':'NOT_EXISTS'}, status=400)
+
+        Review.objects.filter(id=review_id).update(
                 restaurant_id = restaurant_id,
                 user_id       = user.id,
                 description   = data['description'],
                 rating        = data['rating'],
-            )
-
-            ReviewImage.objects.create(
-                review_id = review.id,
-                image     = data['image'],
-            )
-
-            return JsonResponse({'MESSAGE':'SUCCESS'},status = 201)
-
-        except KeyError:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
-
-class ReviewUpdateDeleteView(View):
-    @login_decorator 
-    def put(self, request, restaurant_id, review_id):
-        try:
-            data = json.loads(request.body)
-            user = request.user
-            if not Review.objects.filter(id=review_id).exists():
-                return JsonResponse({'MESSAGE':'NOT_EXISTS'}, status=400)
-
-            Review.objects.filter(id=review_id).update(
-                    restaurant_id = restaurant_id,
-                    user_id       = user.id,
-                    description   = data['description'],
-                    rating        = data['rating'],
-                    )
-
-            ReviewImage.objects.filter(review_id = review_id).update(
-                review_id = review_id,
-                image     = data['image'],
                 )
 
-            return JsonResponse({'MESSAGE':'SUCCESS'}, status = 200)
+        ReviewImage.objects.filter(review_id = review_id).update(
+            review_id = review_id,
+            image     = data['image'],
+            )
 
-        except KeyError:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+        return JsonResponse({'MESSAGE':'SUCCESS'}, status = 200)
 
     @login_decorator
     def delete(self, request, restaurant_id, review_id):
-        try:
-            user = request.user
+        user = request.user
 
-            if not Review.objects.filter(id=review_id).exists():
-                return JsonResponse({'MESSAGE':'NOT_EXISTS'}, status=400)
+        if not Review.objects.filter(id=review_id).exists():
+            return JsonResponse({'MESSAGE':'NOT_EXISTS'}, status=400)
 
-            Review.objects.get(id=review_id).delete()
+        Review.objects.get(id=review_id).delete()
 
-            return JsonResponse({"MESSAGE": 'SUCCESS'}, status=200)
-
-        except KeyError:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+        return JsonResponse({"MESSAGE": 'SUCCESS'}, status=204)
