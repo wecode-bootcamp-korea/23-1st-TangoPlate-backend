7edbfcfa41415bfe8b9fb0c0ac2f82fb90e6ca79
lunayyko <youryoung.ko@gmail.com>
2021-08-11 13:20:57 +0900

    EDIT: review view 관련 내용 삭제

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,6 @@
 from reviews.models import Review
 from django.urls import path
-from reviews.views import ReviewCreateView, ReviewUpdateDeleteView
+
 
 urlpatterns = [
-    path('/<int:restaurant_id>/review', ReviewCreateView.as_view()),
-    path('/<int:restaurant_id>/review/<int:review_id>', ReviewUpdateDeleteView.as_view()),
 ]
