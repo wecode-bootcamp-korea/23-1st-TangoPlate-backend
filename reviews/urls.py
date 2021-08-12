c33384d62d2321b58a2736ab795e37c19bb71221
lunayyko <youryoung.ko@gmail.com>
2021-08-12 10:33:24 +0900

    ADD: review urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
-
+from reviews.views  import ReviewCreateView, ReviewUpdateDeleteView
+from django.urls    import path
 
 urlpatterns = [
+    path('/<int:restaurant_id>/review', ReviewCreateView.as_view()),
+    path('/<int:restaurant_id>/review/<int:review_id>', ReviewUpdateDeleteView.as_view()),
 ]
